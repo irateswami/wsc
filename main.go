112a61a3505038ae209f9e91f47a2bd975edbd0a
irateswami <bryanenglish@protonmail.com>
2022-06-24 11:58:24 -0500

    mildly fleshed out an insert function

--- main.go
+++ main.go
@@ -1,6 +1,66 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/irateswami/wsc/websocket"
+)
+
+var (
+	upgrader = websocket.Upgrader{}
+)
+
+type Cache struct {
+	Mut  *sync.RWMutex
+	Data map[string]*[]byte
+}
+
+func (c *Cache) insert(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("connection upgrade error: ", err)
+	}
+	defer conn.Close()
+
+	for {
+
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("message read error: ", err)
+			continue
+		}
+
+		_ = messageType // switch on message type
+
+		var tempStruct map[string]any
+		if err := json.Unmarshal(message, &tempStruct); err != nil {
+			log.Println("message unmarshall error: ", err)
+			continue
+		}
+
+		newKey := tempStruct["key"].(string)
+		if len(newKey) == 0 {
+			log.Println("key is empty")
+			continue
+		}
+
+		messageBuff := bytes.NewBuffer(message)
+		w := gzip.NewWriter(messageBuff)
+
+		var newBuffer []byte
+		w.Write(newBuffer)
+
+		c.Mut.Lock()
+		c.Data[newKey] = &newBuffer
+		c.Mut.Unlock()
+	}
+}
 
 func main() {
 	fmt.Println("hi")
