6b532926d5f01ca6fa9490fba11621b341ec5252
Bryan English <bryanenglish@pm.me>
2022-06-27 15:22:27 -0500

    much better

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ var (
 	upgrader = websocket.Upgrader{}
 )
 
-type Cache struct {
-	Mut  *sync.RWMutex
+type Database struct {
+	Mut  sync.RWMutex
 	Data map[string][]byte
 }
 
-func (c *Cache) findOne(w http.ResponseWriter, r *http.Request) {
+func (db *Database) insert(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("connection upgrade error: ", err)
@@ -30,12 +30,13 @@ func (c *Cache) findOne(w http.ResponseWriter, r *http.Request) {
 
 	for {
 		messageType, message, err := conn.ReadMessage()
+		if messageType == websocket.CloseMessage {
+			break
+		}
 		if err != nil {
 			log.Println("message read error: ", err)
 			break
 		}
-
-		_ = messageType // switch on message type
 
 		var tempStruct map[string]any
 		if err := json.Unmarshal(message, &tempStruct); err != nil {
@@ -43,149 +44,23 @@ func (c *Cache) findOne(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 
+		//log.Printf("%+v\n", tempStruct)
 		newKey, ok := tempStruct["id"].(string)
 		if !ok {
-			log.Println("id doesn't behave like a string type")
-			break
-		}
-		if len(newKey) == 0 {
-			log.Println("key is empty")
+			log.Println("id conversion failed")
 			break
 		}
 
-		messageBuff := bytes.NewBuffer(message)
-		w := gzip.NewWriter(messageBuff)
+		var compressedBytes bytes.Buffer
+		w := gzip.NewWriter(&compressedBytes)
+		w.Write(message)
 
-		var newBuffer []byte
-		w.Write(newBuffer)
-
-		c.Mut.Lock()
-		c.Data[newKey] = newBuffer
-		c.Mut.Unlock()
-	}
-}
-
-func (c *Cache) returnAll(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("connection upgrade error: ", err)
-	}
-	defer conn.Close()
-
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("message read error: ", err)
-			break
+		db.Mut.Lock()
+		if _, found := db.Data[newKey]; !found {
+			db.Data[newKey] = compressedBytes.Bytes()
+			log.Printf("new id: %s, len: %d", newKey, len(db.Data[newKey]))
 		}
-
-		_ = messageType // switch on message type
-
-		var tempStruct map[string]any
-		if err := json.Unmarshal(message, &tempStruct); err != nil {
-			log.Println("message unmarshall error: ", err)
-			break
-		}
-
-		newKey := tempStruct["id"].(string)
-		if len(newKey) == 0 {
-			log.Println("key is empty")
-			continue
-		}
-
-		messageBuff := bytes.NewBuffer(message)
-		w := gzip.NewWriter(messageBuff)
-
-		var newBuffer []byte
-		w.Write(newBuffer)
-
-		c.Mut.Lock()
-		c.Data[newKey] = newBuffer
-		c.Mut.Unlock()
-	}
-}
-
-func (c *Cache) update(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("connection upgrade error: ", err)
-	}
-	defer conn.Close()
-
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("message read error: ", err)
-			break
-		}
-
-		_ = messageType // switch on message type
-
-		var tempStruct map[string]any
-		if err := json.Unmarshal(message, &tempStruct); err != nil {
-			log.Println("message unmarshall error: ", err)
-			break
-		}
-
-		newKey := tempStruct["id"].(string)
-		if len(newKey) == 0 {
-			log.Println("key is empty")
-			continue
-		}
-
-		messageBuff := bytes.NewBuffer(message)
-		w := gzip.NewWriter(messageBuff)
-
-		var newBuffer []byte
-		w.Write(newBuffer)
-
-		c.Mut.Lock()
-		c.Data[newKey] = newBuffer
-		c.Mut.Unlock()
-	}
-}
-
-func (c *Cache) insert(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Println("connection upgrade error: ", err)
-	}
-	defer conn.Close()
-
-	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("message read error: ", err)
-			break
-		}
-
-		_ = messageType // switch on message type
-
-		var tempStruct map[string]any
-		if err := json.Unmarshal(message, &tempStruct); err != nil {
-			log.Println("message unmarshall error: ", err)
-			break
-		}
-
-		fmt.Printf("%+v\n", tempStruct)
-
-		newKey := tempStruct["id"].(string)
-		if len(newKey) == 0 {
-			log.Println("key is empty")
-			continue
-		}
-
-		messageBuff := bytes.NewBuffer(message)
-		w := gzip.NewWriter(messageBuff)
-
-		var newBuffer []byte
-		w.Write(newBuffer)
-
-		c.Mut.Lock()
-		if _, found := c.Data[newKey]; !found {
-			c.Data[newKey] = newBuffer
-		}
-		c.Mut.Unlock()
+		db.Mut.Unlock()
 	}
 }
 
@@ -195,12 +70,11 @@ func main() {
 	var mut sync.RWMutex
 	data := make(map[string][]byte)
 
-	cache := Cache{
+	db := Database{
 		Data: data,
-		Mut:  &mut,
+		Mut:  mut,
 	}
 
-	http.HandleFunc("/insert", cache.insert)
-	http.HandleFunc("/update", cache.update)
+	http.HandleFunc("/insert", db.insert)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
